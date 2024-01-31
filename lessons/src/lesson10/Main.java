0e47c3d36f1f9fa5a772f4c220cf043574cb2424
WOLarsen <montana.pixel@mail.ru>
2024-01-31 11:55:22 +0400

    change Main

--- lessons/src/lesson10/Main.java
+++ lessons/src/lesson10/Main.java
@@ -5,15 +5,17 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[]args){
-        String s = "Я ищу совпадения в 2024 году. И я их найду в два счёта.";
-        String pattern = "[2024]";
-        Pattern regex = Pattern.compile(pattern);
-        Matcher matcher = regex.matcher(s);
-//        System.out.println(matcher.find());
-        while (matcher.find()){
-//            System.out.println("Шаблон совпадения найден с " + matcher.start() + " по " + (matcher.end() -1));
-            System.out.println(s.substring(matcher.start(), matcher.end()));
-            System.out.println(matcher.group());
-        }
+//        String s = "Я ищу совпадения в 2024 году. И я их найду в два счёта.";
+//        String pattern = "[2024]";
+//        Pattern regex = Pattern.compile(pattern);
+//        Matcher matcher = regex.matcher(s);
+////        System.out.println(matcher.find());
+//        while (matcher.find()){
+////            System.out.println("Шаблон совпадения найден с " + matcher.start() + " по " + (matcher.end() -1));
+//            System.out.println(s.substring(matcher.start(), matcher.end()));
+//            System.out.println(matcher.group());
+//
+//            
+//        }
     }
 }
